b99828d8d2de898056730b8f80c0f56f56df5688
liwencai <1491269442@qq.com>
2022-09-21 16:02:02 +0800

    环境微服务初始化，移除重复依赖

---
+++ kl-environment/src/main/java/com/thtf/environment/KlEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.thtf.environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlEnvironmentApplication.class, args);
+    }
+
+}
---
+++ kl-environment/src/test/java/com/thtf/environment/KlEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thtf.environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
