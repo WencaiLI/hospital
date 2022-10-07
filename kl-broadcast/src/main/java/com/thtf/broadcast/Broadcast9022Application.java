99cb2de2457a4d2c89bc6c1303d233061ba0ae4f
liwencai <1491269442@qq.com>
2022-10-07 14:04:49 +0800

    广播微服务初始化

---
+++ kl-broadcast/src/main/java/com/thtf/broadcast/Broadcast9022Application.java
@@ -0,0 +1,13 @@
+package com.thtf.broadcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Broadcast9022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Broadcast9022Application.class, args);
+    }
+
+}
---
+++ kl-broadcast/src/main/java/com/thtf/broadcast/service/BroadcastService.java
@@ -0,0 +1,9 @@
+package com.thtf.broadcast.service;
+
+/**
+ * @Auther: liwencai
+ * @Date: 2022/10/7 13:41
+ * @Description:
+ */
+public interface BroadcastService {
+}
