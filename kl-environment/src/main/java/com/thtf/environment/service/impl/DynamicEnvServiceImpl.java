64c39cb168826dc88caa32958967920f831596d6
liwencai <liwencai@thtf.com.cn>
2022-10-09 09:51:02 +0800

    动环监控初始化;

---
+++ kl-environment/src/main/java/com/thtf/environment/service/impl/DynamicEnvServiceImpl.java
@@ -0,0 +1,13 @@
+package com.thtf.environment.service.impl;
+
+import com.thtf.environment.service.DynamicEnvService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: liwencai
+ * @Date: 2022/10/8 18:04
+ * @Description:
+ */
+@Service
+public class DynamicEnvServiceImpl implements DynamicEnvService {
+}
