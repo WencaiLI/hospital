cf137190f36d9256828b1d820fcc4dc45154f282
liwencai <liwencai@thtf.com.cn>
2022-10-09 15:19:04 +0800

    bug修复

--- kl-office/src/main/java/com/thtf/office/OfficeServerMain9001.java
+++ kl-office/src/main/java/com/thtf/office/OfficeServerMain9001.java
@@ -3,6 +3,8 @@ package com.thtf.office;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,8 +17,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableFeignClients("com.thtf.common")
 @ComponentScan(basePackages = {"com.thtf.office","com.thtf.common"})
-public class OfficeServerMain9001 {
+public class OfficeServerMain9001 extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(OfficeServerMain9001.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OfficeServerMain9001.class);
     }
 }
