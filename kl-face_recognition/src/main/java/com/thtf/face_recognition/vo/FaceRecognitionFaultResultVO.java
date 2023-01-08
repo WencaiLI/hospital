5c89a795e1a7c2e4b155102b7f9e3050cc7ef350
liwencai <1491269442@qq.com>
2023-01-08 17:45:23 +0800

    修改人脸识别逻辑

--- kl-face_recognition/src/main/java/com/thtf/face_recognition/vo/FaceRecognitionFaultResultVO.java
+++ kl-face_recognition/src/main/java/com/thtf/face_recognition/vo/FaceRecognitionFaultResultVO.java
@@ -1,7 +1,10 @@
 package com.thtf.face_recognition.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -41,6 +44,13 @@ public class FaceRecognitionFaultResultVO {
      * 报警级别
      */
     private Integer alarmLevel;
+
+    /**
+     * 报警时间
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private LocalDateTime alarmTime;
 
     /**
      * 留置时长
